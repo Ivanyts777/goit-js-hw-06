9890b5acd495df7e5ab65fbf097fc2f9a8409a25
Volodymyr Ivanuts <Ivanyts777@gmail.com>
2020-07-09 17:37:48 +0300

    md-06

---
+++ js/task-03.js
@@ -0,0 +1,11 @@
+"use strict";
+import * as allUsers from "./users.js";
+let users = allUsers.default;
+
+const getUsersWithGender = (users, gender) => {
+  return users
+    .filter((user) => user.gender === gender)
+    .map((user) => user.name);
+};
+
+console.log(getUsersWithGender(users, "male")); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
---
+++ js/task-04.js
@@ -0,0 +1,9 @@
+"use strict";
+import * as allUsers from "./users.js";
+let users = allUsers.default;
+
+const getInactiveUsers = (users) => {
+  return users.filter((user) => !user.isActive);
+};
+
+console.log(getInactiveUsers(users)); // [об'єкт Moore Hensley, об'єкт Ross Vazquez, об'єкт Blackburn Dotson]
